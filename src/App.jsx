03b039f8ae9c8f66828bef3bbbc24fd88bf7c9a0
Israel Pina <israelpina047@gmail.com>
2024-11-29 23:45:51 -0300

    build : add new components for thee APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
+import { ThemeProvider } from "./contexts/index"
+import { Home } from "../src/Home/index"
+import "./App.css"
 
 function App() {
   return (
-    <>
-     
-    </>
+    <div>
+      <ThemeProvider >
+        <Home />
+      </ThemeProvider>
+    </div>
   )
 }
 
