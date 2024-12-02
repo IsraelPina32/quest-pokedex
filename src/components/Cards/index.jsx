573457e518ee3ac8c306157963f83eacf0fd6a36
Israel Pina <israelpina047@gmail.com>
2024-12-02 16:18:09 -0300

    feat : add new CardPokemons with PokeAPI and test

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,12 +1,43 @@
-
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 export const Cards = () => {
+    const [pokemons, setPokemons] = useState([]);
+    useEffect(() => {
+        async function getPokemons() {
+            try {
+                const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+                const data = await response.json();
+
+                const details = await Promise.all(data.results.map(async (pokemon) => {
+                    const pokemonsDetails = await fetch(pokemon.url);
+                    return pokemonsDetails.json();
+
+                })
+                );
+                setPokemons(details);
+            } catch (error) {
+                console.log("Seu Pokemon não foi encontrado", error);
+
+            }
+        };
+        getPokemons();
+    }, [])
     return (
         <div>
-            <div className="card">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="pokemon" />
-                <h2>Charmander</h2>
-                <p>Charmander is a Fire-type Pokémon. It is a medium-sized pokemon that has a long tail and a long body.</p>
-            </div>
+             {pokemons.map((pokemon) => (
+                <div key={pokemon.name}>
+                    <h3>{pokemon.name}</h3>
+                    <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
+                    {pokemon.types.map((type) => (
+                        <span key={type.type.name}>{type.type.name}</span>
+                    ))}
+                </div>
+             ))}
         </div>
-    )
+
+    );
 }
+
+const StyleCard = styled.div`
+    display: flex;
+`
