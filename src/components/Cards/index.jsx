d558a7a5516f7b04e371ada5f43266c9893aba98
Israel Pina <israelpina047@gmail.com>
2024-12-04 00:42:44 -0300

    feat : add new functions for my components Cards and ThemeToogleButton

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,43 +1,109 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import getTypeColors from "../../utils/GetTypeColors";
+
+
+const StyleCard = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    padding: 1rem;
+`
+
+const StyleCardsPokemons = styled.div`
+    display: flex;
+    max-width: 750px;
+    width: 100%;
+    gap: 1rem;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: center;
+    text-align: center;
+    border-radius: 1rem;
+    background-color: #008393;
+    padding: 1rem;
+`
+
+const StyleCardPokemon = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 0.5rem;
+    padding: 0.6rem;
+    width: 150px;
+    height: 200px;
+    border-radius: 0.5rem;
+    background-color: #C2D5E4;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        transform: scale(1.1);
+    }
+`
+
+const StyleTitlePokemons = styled.div`
+     padding: 0 1rem;
+     font-size: 1.2rem;
+     font-weight: bold;
+`
+const StylePhrase = styled.div`
+    width: fit-content;
+    padding: 0.1rem 0.6rem;
+    border-radius: 0.8rem;
+    font-size: 0.7rem;
+    font-weight: 700;
+    background-color: ${({ type }) => getTypeColors(type)}
+`
 export const Cards = () => {
     const [pokemons, setPokemons] = useState([]);
-    useEffect(() => {
-        async function getPokemons() {
+    const [offset, setOffset] = useState(0);
+    const [loading , setLoading] = useState(false);
+        async function getPokemons(offset) {
+            setLoading(true);
             try {
-                const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon?&offset=${offset}`);
+                if(!response.ok) {
+                    throw new Error("Seu Pokemon não foi encontrado");
+                }
                 const data = await response.json();
 
                 const details = await Promise.all(data.results.map(async (pokemon) => {
                     const pokemonsDetails = await fetch(pokemon.url);
                     return pokemonsDetails.json();
-
-                })
-                );
-                setPokemons(details);
+                }));
+                setPokemons((prevPokemons) => [...prevPokemons, ...details]);
             } catch (error) {
                 console.log("Seu Pokemon não foi encontrado", error);
-
+            } finally {
+                setLoading(false);
             }
         };
-        getPokemons();
-    }, [])
+        const handleLoadMore = () => {
+            setOffset((prevOffset) => prevOffset + 10);
+        };
+
+        useEffect(() => {
+            if(offset >= 0){
+                getPokemons(offset);
+            }
+        }, [offset])
     return (
         <div>
-             {pokemons.map((pokemon) => (
-                <div key={pokemon.name}>
-                    <h3>{pokemon.name}</h3>
-                    <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
-                    {pokemon.types.map((type) => (
-                        <span key={type.type.name}>{type.type.name}</span>
+            <StyleCard>
+                <StyleCardsPokemons>
+                    {pokemons.map((pokemon) => (
+                        <StyleCardPokemon key={pokemon.id} type={pokemon.types[0]?.type.name}>
+                            <img src={pokemon.sprites.front_default} alt={` Imagem do Pokemon ${pokemon.name} `} />
+                            <StyleTitlePokemons>{pokemon.name}</StyleTitlePokemons>
+                            {pokemon.types.map((type) => (
+                                <StylePhrase key={type.type.name} type={type.type.name}>{type.type.name}</StylePhrase>
+                            ))}
+                        </StyleCardPokemon>
                     ))}
-                </div>
-             ))}
+                </StyleCardsPokemons>
+            </StyleCard>
+            <button onClick={handleLoadMore } disabled={loading}>Carregar mais</button>
         </div>
-
     );
 }
-
-const StyleCard = styled.div`
-    display: flex;
-`
--- src/components/Theme-toogle-button/index.jsx
+++ src/components/Theme-toogle-button/index.jsx
@@ -1,13 +1,19 @@
 import { useContext } from "react"
 import { ThemeContext, themes } from "../../contexts/index";
+import styled from "styled-components";
 
+const StyleButtonTheme = styled.button`
+    background-color: #FE4B00;
+    color: #FAFAFA;
+    border: none;
+    padding: 0.6rem;
+    border: 1px solid #FBD570;
+    border-radius: 0.5rem;
+    cursor: pointer;
+`
 export const ThemeToogleButton = () => {
 
     const { theme, setTheme } = useContext(ThemeContext)
 
-    return (
-        <>
-            <button onClick={() => setTheme(theme === themes.light ? themes.dark : themes.light)}>Mude o tema</button>
-        </>
-    )
+    return <StyleButtonTheme onClick={() => setTheme(theme === themes.light ? themes.dark : themes.light)}>Mude o tema</StyleButtonTheme>
 }
