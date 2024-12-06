c80101700475e42607b0be43c3848f3e616d7306
Israel Pina <israelpina047@gmail.com>
2024-12-05 22:00:56 -0300

    style : remove the space

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -2,7 +2,6 @@ import { Cards } from "../../src/components/Cards/index"
 import { Navbar } from "../../src/components/Navbar/index"
 import { ThemeContext } from "../../src/contexts/index"
 import { useContext } from "react"
-import { Button } from "../components/Button"
 import styled from "styled-components"
 
 const StyleHomePage = styled.div`
