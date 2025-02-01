18d00cdb2ddaaaf62c88bd76a67683290a559d62
Israel Pina <israelpina047@gmail.com>
2025-01-31 23:44:03 -0300

    feat : add new setError

--- src/components/CardsDetails/index.jsx
+++ src/components/CardsDetails/index.jsx
@@ -113,6 +113,7 @@ export const CardsDetails = () => {
         const fetchPokemon = async () => {
             try {
                 setLoading(true)
+                setError(null)
                 const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
                 if (!response.ok) {
                     throw new Error(`Error de requisição : ${response.status}`);
