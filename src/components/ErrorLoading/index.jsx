dce330024bb38abb686e6538988d0cd1ec0a9977
Israel Pina <israelpina047@gmail.com>
2025-02-10 01:18:18 -0300

    refactor : add new component for ErrorLoading

---
+++ src/components/ErrorLoading/index.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ClipLoader } from 'react-spinners';
+import { useNavigate } from 'react-router-dom';
+
+const StyleCardDefault = styled.div`
+    height: 100vh;
+    font-size: 1.2rem;
+    font-weight: 600;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: ${({ theme }) => theme.color};
+    background: ${({ theme }) => theme.background};
+`;
+
+const StyleH1 = styled.h1`
+    font-size: 2.2rem;
+    font-weight: bold;
+    margin-top: 1.5rem;
+    color: ${({ theme }) => theme.color};
+`;
+
+const StyleImg = styled.img`
+    width: 300px;
+    height: 250px;
+    transition: all 0.3s ease-in-out;
+    border-radius: 0.5rem;
+
+    &:hover {
+        transform: scale(1.1);
+    }
+`;
+
+const StyleButtonError = styled.button`
+    padding: 0.8rem;
+    margin-top: 0.5rem;
+    border: 1px solid #2b2d42;
+    background: none;
+    background-color: #ef233c;
+    box-shadow: 0 0 0.5em #8d99ae;
+    border-radius: 0.5rem;
+    cursor: pointer;
+    font-weight: bold;
+    margin-top: 1.5rem;
+`;
+
+
+export const ErrorLoading = ({ loading, error, theme }) => {
+
+    const navigate = useNavigate();
+
+    if (loading) {
+        return (
+            <StyleCardDefault theme={theme}>
+                <ClipLoader theme={theme} color="#007ea7" loading={loading} size={150} />
+                <StyleH1>Buscando o seu Pokemon, aguarde...</StyleH1>
+            </StyleCardDefault>
+        );
+    }
+
+    if (error) {
+        return (
+            <StyleCardDefault theme={theme}>
+                <StyleImg src="/assets/not-found.png" alt="Erro 404" />
+                <StyleH1 theme={theme}>Error Not Found 404 </StyleH1>
+                <StyleButtonError onClick={() => navigate("/")}>Volte para a Pagina Inicial</StyleButtonError>
+            </StyleCardDefault>
+        );
+    }
+
+    return null;
+};
