ea658a0aa0c5e26406e0929f855a9aa2cc4ce8e6
IsraelPina32 <israelpina047@gmail.com>
2025-11-13 17:04:37 -0300

    feat : adicionar o typescript no main.tsx adicionar checagem para verificar de forma segura o DOM.

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { GlobalStyle } from './styles/index'
 import App from './App.js'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+const container = document.getElementById('root');
+
+if(!container) {
+  throw new Error('Root container missing in DOM.');
+}
+
+const root = createRoot(container);
+
+root.render(
+   <StrictMode>
     <GlobalStyle/>
     <App />
   </StrictMode>,
-)
+);
+
