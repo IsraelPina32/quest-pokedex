6cffcc9d6723ae7c643403a2987f5eedbfaa2711
IsraelPina32 <israelpina047@gmail.com>
2025-11-20 20:00:13 -0300

    style(CardsDetails): Adicionar novo estilo para o componente CardsDetails e nova foco para o fundo do card.

--- src/components/CardsDetails/index.tsx
+++ src/components/CardsDetails/index.tsx
@@ -1,10 +1,10 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { ButtonBack } from "../ButtonBack/index.js";
 import styled from "styled-components";
 import { ThemeContext } from "../../contexts/index.js";
 import { useContext } from "react";
 import { ErrorLoading } from "../ErrorLoading/index";
-import getTypeColors from "../../utils/GetTypeColors.js";
+import {getTypeColor, getPokemonBackground} from "../../utils/GetTypeColors.js";
 import { usePokemonByName } from "../../hooks/usePomeonsByName.js";
 
 const StyleContainer = styled.div`
@@ -25,37 +25,42 @@ const StyleCardDetails = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: 2rem;
+    padding: 1rem;
     list-style: none;
     font-weight: 500;
     font-family: 'Roboto', sans-serif;
-    margin: 0 auto;
-    @media (max-width: 425px) {
-       padding: 6rem 0 0.7rem 0;
-    }  
+    margin: 0 auto; 
 `
-const StyleCardPokemon = styled.div<{ type: string }>`
+const StyleCardPokemon = styled.div<{ $pokemonTypes: string[]}>`
     padding: 1rem;
     height: 80%;
     width: 650px;
-     background-image: 
-            linear-gradient(${({ type }) => getTypeColors(type)}, ${({ type }) => getTypeColors(type)}),
-            linear-gradient(45deg, gold, #ffd700, gold);
     border-radius: 0.7rem;
-    background-origin: border-box;
-    background-clip: padding-box, border-box;
-    box-shadow: 0 4px 12px rgba(0,0,0,0.3);
-
-     @media (max-width: 425px) {
-        height: 95%;
+    padding: 2rem 1rem;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    background: ${({ $pokemonTypes }) => getPokemonBackground($pokemonTypes)}; 
+       backdrop-filter: blur(5px);
+       -webkit-backdrop-filter: blur(10px);
+       box-shadow: 
+           0 8px 32px 0 rgba(0, 0, 0, 0.3),
+           inset 0 0 20px rgba(255, 255, 255, 0.2);
+   
+       transition: all 0.4s cubic-bezier(0.25, 0.8, 0.25, 1);
+       z-index: 1;
+   
+       &:hover {
+           transform: translateY(-8px);
+           
+           border-color: rgba(255, 255, 255, 0.9);
+           box-shadow: 
+               0 15px 45px 0 rgba(0, 0, 0, 0.4),
+               inset 0 0 30px rgba(255, 255, 255, 0.4);
+       }
+    @media (max-width: 425px) {
+        width: 90%;
+        margin-top: 40px;
     }
-`
-const StyleH1 = styled.h1`
-    font-family: 'PressStart2P', monospace;
-    font-size: 2.2rem;
-    font-weight: bold;
-    margin-top: 1.5rem;
-    color: ${({theme}) => theme.color};
+    
 `
 const StyleImg = styled.img`
     width: 300px;
@@ -66,19 +71,33 @@ const StyleImg = styled.img`
     &:hover {
         transform: scale(1.1);
     }
+    @media (max-width: 425px) {
+        width: 250px;
+        height: 200px;
+    }
 `
+const StyleH1 = styled.h1`
+    font-family: 'PressStart2P', monospace;
+    font-size: 1.9rem;
+    font-weight: bold;
+    margin-top: 2rem;
+    color: ${({theme}) => theme.color};
+`
+
 const StyleSkills = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    gap: 0.8rem;
+    gap: 1rem;
     padding: 1.3rem;
-    border-radius: 0.3rem;
+    border-radius: 0.8rem;
     background: url("/assets/background-card.jpg") no-repeat center center;
+    box-shadow: 
+        0 8px 32px 0 rgba(0, 0, 0, 0.3),
+        inset 0 0 20px rgba(255, 255, 255, 0.2);
     background-size: cover;
     object-fit: center;
-    border: 1px solid #007ea7;
     position: relative;
     overflow: hidden;
 
@@ -89,7 +108,7 @@ const StyleSkills = styled.div`
         left: 0;
         right: 0;
         bottom: 0;
-        background: rgba(76, 74, 74, 0.5);
+        background: rgba(109, 106, 106, 0.5);
         z-index: 1;
     }
 
@@ -99,17 +118,17 @@ const StyleSkills = styled.div`
     }
 `
 const StyleP = styled.p`
-    font-size: 1.3rem;
-    font-weight: 900;
-    margin-bottom: 0.7rem;
-    border-bottom: 1px solid #9ef01a;
+    font-size: 1.8rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+    border-bottom: 1px solid #e2b934ff;
     width: 100%;
 `
 const StyleList = styled.ul`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 0.9rem;
+    gap: 1rem;
 `
 const StyleL = styled.li<{ type: string }>`
     font-size: 1.2rem;
@@ -118,35 +137,11 @@ const StyleL = styled.li<{ type: string }>`
     border-radius: 0.8rem;
     font-weight: 900;
     list-style: none;
-    background-color: ${({ type }) => getTypeColors(type)}
-`
-const StyleButton = styled.button`
-    padding: 0.5rem;
-    margin-top: 0.5rem;
-    border: 1px solid #4d908e;
-    background: none;
-    background-color: #34a0a4;
-    box-shadow: 0 0 0.5em #4d908e;
-    border-radius: 0.5rem;
-    cursor: pointer;
-    position: relative;
-    top: 35px;
-    right: 150px;
-    
-     @media (max-width: 425px) {
-        right: 125px;
-        top: 70px;
-    }
+    background-color: ${({ type }) => getTypeColor(type)}
 `
 
-
-const StyleArrowReturn = styled.img`
-    height: 30px;
-    width: 30px;
-`
 export const CardsDetails = () => {
     const { name  } = useParams<{name: string}>();
-    const navigate = useNavigate();
     const {pokemon, loading, error} = usePokemonByName(name);
     const { theme } = useContext(ThemeContext);
 
@@ -158,12 +153,12 @@ export const CardsDetails = () => {
                   <StyleContainer style={{ color: theme?.color, background: theme?.background }}>
                   <StyleCardDetails>
                          <ButtonBack/>
-                      <StyleCardPokemon type={pokemon.types?.[0].type?.name}>
+                      <StyleCardPokemon key={pokemon.id} $pokemonTypes={pokemon.types.map((type: any) => type.type.name)}>
                           <StyleH1>{pokemon.name}</StyleH1>
                           <StyleImg src={pokemon.sprites?.front_default || 'url_not_found'} alt={pokemon.name} />
                           <StyleSkills>
-                              <StyleP>Altura: {(pokemon.height / 10).toFixed(1)} m</StyleP>
-                              <StyleP>Peso: {(pokemon.weight / 10).toFixed(1)} kg</StyleP>
+                              <StyleP>Altura: {(pokemon.height / 10).toFixed(1)} M</StyleP>
+                              <StyleP>Peso: {(pokemon.weight / 10).toFixed(1)} KG</StyleP>
                               <StyleP>Tipo(s) </StyleP>
                               <StyleList> {pokemon.types?.length > 0 ? (
                                   pokemon.types.map((type: any) => (
